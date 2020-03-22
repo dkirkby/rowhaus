61553256d42a5ed6ad8ad938f5e5dbc4cb272ba1
David Kirkby <dkirkby@uci.edu>
2020-03-21 18:40:03 -0700

    Run subprocess filling queue

--- driver/driver.py
+++ driver/driver.py
@@ -1,37 +1,43 @@
 import time
 import RPi.GPIO as GPIO
+from multiprocessing import Process, Queue
 
-# Use BCM numbering so we can access the on-board LEDs
-GPIO.setmode(GPIO.BCM)
+def monitor_switch(queue):
+    SWITCH = 5
+    GPIO.setmode(GPIO.BOARD)
+    # Channel has a hardware pullup.
+    GPIO.setup(SWITCH, GPIO.IN)
 
-SWITCH = 3 # BCM numbering
+    count = 0
+    last_time = [0, time.time()]
+    last_state = -1
 
-# Channel has a hardware pullup.
-GPIO.setup(SWITCH, GPIO.IN)
+    def edge(pin):
+        nonlocal count, last_time, last_state
+        new_state = GPIO.input(SWITCH)
+        new_time = time.time()
+        if new_state == 1 and last_state == 0:
+            # Calculate elapsed times.
+            payload = count, new_time - last_time[1], new_time - last_time[0]
+            queue.put(payload)
+            count += 1
+        last_time[new_state] = new_time
+        last_state = new_state
 
-count = 0
-last_time = [0, time.time()]
-last_state = -1
+    GPIO.add_event_detect(SWITCH, GPIO.BOTH, callback=edge, bouncetime=1)
 
-def edge(pin):
-    global count, last_time, last_state
-    new_state = GPIO.input(SWITCH)
-    new_time = time.time()
-    if new_state == 1 and last_state == 0:
-        # Calculate elapsed times.
-        elapsed = new_time - last_time[1], new_time - last_time[0]
-        print(count, elapsed)
-        count += 1
-    last_time[new_state] = new_time
-    last_state = new_state
-
-GPIO.add_event_detect(SWITCH, GPIO.BOTH, callback=edge, bouncetime=1)
-
-try:
-    print('Hit ^C to exit')
     while True:
-        #print("tick")
-        time.sleep(1)
-except KeyboardInterrupt:
-    print('bye')
+        time.sleep(10)
 
+        
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=monitor_switch, args=(q,))
+    p.start()
+    try:
+        while True:
+            count, t1, t2 = q.get()
+            print(count, t1, t2)
+    except KeyboardInterrupt:
+        p.join()
+        print('bye')
