4d591ac76d273713af4a370e5a71391f687fddbd
David Kirkby <dkirkby@uci.edu>
2020-03-23 17:46:00 -0700

    Implement SPM calculation

--- static/rowhaus.js
+++ static/rowhaus.js
@@ -1,12 +1,16 @@
 var start_time = null;
 var count = null;
+var last_dt1 = null;
+var dt1_history = null;
+const max_history = 100;
+const avg_cycles = 6;
 
 function zeropad(value) {
     return (value < 10) ? "0" + value : "" + value;
 }
 
 function update_time() {
-    var t = new Date() - start_time;
+    let t = new Date() - start_time;
     $("#hours").text(zeropad(Math.floor( (t/(1000*60*60)) % 24)));
     $("#mins").text(zeropad(Math.floor( (t/1000/60) % 60)));
     $("#secs").text(zeropad(Math.floor( (t/1000) % 60)));
@@ -15,6 +19,19 @@ function update_time() {
 function update_data(data) {
     count++;
     $("#COUNT").text(Math.round(count/2, 0));
+    if(dt1_history.length == max_history) {
+        // Remove the oldest entry from the front.
+        dt1_history.shift();
+    }
+    // Append the newest entry to the end.
+    dt1_history.push(data.dt1);
+    if(dt1_history.length >= avg_cycles) {
+        // Use the recent cycles to calculate the running SPM.
+        let sum = dt1_history.slice(-avg_cycles).reduce(
+            (acc, val) => acc + val, 0);
+        let spm = 30 * avg_cycles / sum;
+        $("#SPM").text(spm.toFixed(1));
+    }
 }
 
 function fetch() {
@@ -24,7 +41,6 @@ function fetch() {
         dataType: 'json',
         timeout: 250,
         success: function(data) {
-            console.log(data);
             if(data.dt1 != undefined) {
                 update_data(data);
             }
@@ -43,6 +59,8 @@ function reset() {
             update_time();
             count = 0;
             $("#COUNT").text('0');
+            $("#SPM").text('0.0');
+            dt1_history = [];
         }
     });
     console.log('reset at', start_time);
@@ -51,8 +69,8 @@ function reset() {
 function run() {
     reset();
     $("#reset").click(reset);
-    var fetcher = setInterval(fetch, 1000);
-    var timer = setInterval(update_time, 1000);
+    let fetcher = setInterval(fetch, 1000);
+    let timer = setInterval(update_time, 1000);
 }
 
 $(document).ready(run);
